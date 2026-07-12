3525b224456aff68068d60960519e48445873c3d
Sergey <xserge@ukr.net>
2026-07-12 14:52:13 +0300

    Обновление версии 5.1.6

--- sync.bat
+++ sync.bat
@@ -2,10 +2,10 @@
 setlocal
 
 :: 1. Ищем номер версии в главном скрипте (заменить main.py на имя главного скрипта проекта). 
-::set verFile=xtail.py
+set verFile=xtail.py
 
 :: 2. Извлекаем версию
-::for /f "usebackq tokens=*" %%a in (`python -c "import re; f=open('%verFile%', encoding='utf-8'); print(re.search(r'__version__\s*=\s*[\x27\x22]([^\x27\x22]+)', f.read()).group(1))"`) do (set vers=%%a)
+for /f "usebackq tokens=*" %%a in (`python -c "import re; f=open('%verFile%', encoding='utf-8'); print(re.search(r'__version__\s*=\s*[\x27\x22]([^\x27\x22]+)', f.read()).group(1))"`) do (set vers=%%a)
 set vers="5.1.6"
 
 :: 3. Формируем сообщение коммита
